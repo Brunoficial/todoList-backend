9f3208f7ccb66663ee35800ff89f9332800a7def
Brunoficial <bruno.lopes.vrls@gmail.com>
2025-07-25 11:41:36 -0300

    fix: Corrigidas as respostas das rotas de Task, e começada a rota de updateTask

--- src/main/java/com/java_toDo_list/services/TaskService.java
+++ src/main/java/com/java_toDo_list/services/TaskService.java
@@ -4,11 +4,15 @@ import com.java_toDo_list.DTOs.TaskDto;
 import com.java_toDo_list.mappers.TaskMapper;
 import com.java_toDo_list.models.TaskModel;
 import com.java_toDo_list.repositories.TaskRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import java.lang.reflect.Field;
+
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -16,16 +20,30 @@ public class TaskService {
     @Autowired
     TaskRepository taskRepository;
 
-    public List<TaskModel> listTasks() {
+    @Transactional
+    public ResponseEntity<List<TaskModel>> listTasks() {
         List<TaskModel> tasks =  taskRepository.findAll();
-        return tasks;
+        return ResponseEntity.ok(tasks);
     }
 
-    public TaskModel createTask(TaskDto data) {
+    @Transactional
+    public ResponseEntity<TaskModel> createTask(TaskDto data) {
         TaskModel newTask = TaskMapper.mapToTaskModel(data);
-        System.out.println("ID antes de salvar: " + newTask.getId());
         taskRepository.save(newTask);
 
-        return newTask;
+        return ResponseEntity.ok(newTask);
+    }
+
+    public ResponseEntity<Void> deleteTask(Long id) {
+        taskRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    public TaskModel updateTask(TaskDto data, Long id) {
+        TaskModel task = taskRepository.findById(id);
+         Field[] campos = task.getClass().getDeclaredFields();
+         for (Field campo : campos) {
+             campo.setAccessible(true);
+         }
     }
 }
